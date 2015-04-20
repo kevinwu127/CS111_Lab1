2cfdad1fd2dc380d1d1c813dbd59abec2f5eee6f
Michelle Wang <michellewang@Cs-213-59.CS.UCLA.EDU>
2015-04-20 14:58:18 -0700

    1B

--- execute-command.c
+++ execute-command.c
@@ -3,10 +3,16 @@
 #include "command.h"
 #include "command-internals.h"
 
+#include <unistd.h>
 #include <error.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
+
+void execute_simple(command_t c);
+void execute_non_redir(command_t c, bool or_command);
 
 int
 command_status (command_t c)
@@ -20,5 +26,55 @@ execute_command (command_t c, bool time_travel)
   /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
-  error (1, 0, "command execution not yet implemented");
+ 	
+ 	switch (c->type)
+ 	{
+ 		case SIMPLE_COMMAND:
+ 		{
+ 			execute_simple(c);
+ 			break;
+ 		}
+ 		case AND_COMMAND:
+ 		case OR_COMMAND:
+ 		case SEQUENCE_COMMAND:
+ 		{
+			execute_non_redir(c, c->type == OR_COMMAND);
+			break;
+ 		}
+ 		default:
+ 		{
+ 			abort();
+ 		}
+
+ 	}
+}
+
+void
+execute_simple(command_t c)
+{
+	char ** cmd = c->u.word;
+	execvp(cmd[0], cmd);
+	exit(127);								// if command FAILS, manually exit 127
+}
+
+void
+execute_non_redir(command_t c, bool or_command)
+{
+	int p = fork();
+	if (p == 0)
+	{
+		execute_command(c->u.command[0], 0);
+	}
+	else
+	{
+		int status;								// stores not only the exit status, but a lot of other information 											  too.
+												// need to extract bits for exit status, LEAST SIGNIFICANT 8 BITS
+		waitpid(p, &status, 0);					// blocking approach
+		int exit_status = WEXITSTATUS(status);	// gets exit status from status
+		if ( (exit_status == 1 && or_command) ||
+			 (exit_status == 0 && !or_command) )
+		{
+			execute_command(c->u.command[1], 0);
+		}
+	}
 }
